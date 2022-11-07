930aa58adae999a302dafdeecde467ad735512ee
Kar Martikyan <kamartikyan@gmail.com>
2022-11-07 15:20:39 +0800

    Initial commit

---
+++ worker.js
@@ -0,0 +1,20 @@
+const checkInterval = 100;
+
+setInterval(() =>
+{
+    let panels = document.getElementsByClassName("game-panel-btns-container board-panel-game-panel-wrapper")
+    if (panels == null || panels.length == 0)
+    {
+        return;
+    }
+
+    let claimButtons = document.getElementsByClassName("ui_v5-button-component ui_v5-button-secondary ui_v5-button-x-small");
+    if (claimButtons == null || claimButtons.length == 0)
+    {
+        return;
+    }
+
+    var button = claimButtons[0];
+    console.log("Claim button found");
+    button.click();
+}, checkInterval);
